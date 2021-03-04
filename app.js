86301eb480a7e095351605a5cd16458fbf9a32b7
Aishwarya Singh <aishwarya@Aishwaryas-MacBook-Air.local>
2021-03-04 16:08:27 +0800

    First

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const socket = require('socket.io')
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+var port = process.env.PORT || 3000;
+
+//function sleepFor( sleepDuration ){
+  //var now = new Date().getTime();
+  //while(new Date().getTime() < now + sleepDuration){ /* do nothing */ } 
+//}
+
+//Render Index page
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+//Get username and roomname from form and pass it to room
+app.post('/room', (req, res) => {
+    roomname = req.body.roomname;
+    username = req.body.username;
+    res.redirect(`/room?username=${username}&roomname=${roomname}`)
+})
+
+//Rooms
+app.get('/room', (req, res)=>{
+    res.render('room')
+})
+
+//Start Server
+const server = app.listen(port, () => {
+    console.log(`Server Running on ${port}`)
+})
+
+const io = socket(server);
+require('./utils/socket')(io);
+
